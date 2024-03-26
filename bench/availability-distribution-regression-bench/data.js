036504e0b04e104a2f3620f701a983d06fc5b478
github-action-benchmark <github@users.noreply.github.com>
2024-03-26 16:37:54 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 0b868cf3f9b6ee3c6d4976594e30eb7345bd7b0c

---
+++ bench/availability-distribution-regression-bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711471074864,
+  "repoUrl": "https://github.com/paritytech-stg/alvicsam-playground",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "alvicsam",
+            "username": "alvicsam",
+            "email": "alvicsam@gmail.com"
+          },
+          "committer": {
+            "name": "alvicsam",
+            "username": "alvicsam",
+            "email": "alvicsam@gmail.com"
+          },
+          "id": "0b868cf3f9b6ee3c6d4976594e30eb7345bd7b0c",
+          "message": "fix bench path",
+          "timestamp": "2024-03-26T16:37:05Z",
+          "url": "https://github.com/paritytech-stg/alvicsam-playground/commit/0b868cf3f9b6ee3c6d4976594e30eb7345bd7b0c"
+        },
+        "date": 1711471074448,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 1.9,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 307302,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.2,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 12.7,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
