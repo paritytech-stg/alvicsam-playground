2376092e456e3882320cf22109b81bfb6b91cc96
alvicsam <alvicsam@gmail.com>
2024-05-16 16:37:58 +0200

    test stopper

---
+++ stopper.py
@@ -0,0 +1,56 @@
+import os
+import re
+import sys
+
+from github import Github
+
+try:
+    access_token = os.getenv("GITHUB_TOKEN")
+    sha = os.getenv("GITHUB_SHA")
+    gh_repo = os.getenv("GITHUB_REPOSITORY")
+    pr_num = os.getenv("GITHUB_REF_NAME").split("/")[0]
+except KeyError as error:
+    print(f"{error} not found in environment variables")
+    sys.exit(1)
+
+
+def get_workflow_id(url):
+    """
+    Extracts the workflow ID from a given URL.
+
+    Args:
+        url (str): The URL containing the workflow ID.
+
+    Returns:
+        str or None: The extracted workflow ID if found, None otherwise.
+    """
+    pattern = r"/actions/runs/(\d+)/"
+    match = re.search(pattern, url)
+    if match:
+        return match.group(1)
+    return None
+
+
+if __name__ == "__main__":
+    g = Github(access_token)
+    repo = g.get_repo(gh_repo)
+    commit = repo.get_commit(sha=sha)
+    check_runs = commit.get_check_runs()
+    workflow_ids = []
+
+    for check_run in check_runs:
+        id = get_workflow_id(check_run.html_url)
+        if id:
+            workflow_ids.append(id)
+
+    workflow_ids = list(set(workflow_ids))
+    print(workflow_ids)
+
+    # stop all running workflows
+    for workflow_id in workflow_ids:
+        workflow = repo.get_workflow_run(int(workflow_id))
+        if workflow.status == "in_progress" or workflow.status == "queued":
+            workflow.cancel()
+            print(f"Workflow {workflow_id} has been cancelled.")
+        else:
+            print(f"Workflow {workflow_id} is not running.")
